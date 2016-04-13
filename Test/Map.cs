0062d0241b73089f470b3511ef341b6c3722d123
Wano-k <mmarie.laporte@gmail.com>
2016-04-13 02:03:58 +0200

    GC correction

--- Test/Map.cs
+++ Test/Map.cs
@@ -13,12 +13,13 @@ namespace Test
     class Map
     {
         GraphicsDevice device;
-        List<VertexPositionTexture> vertexList;
+        List<VertexPositionTexture> verticesList;
         List<int> indexesList;
+        VertexPositionTexture[] verticesArray;
+        int[] indexesArray;
         VertexBuffer vb;
         IndexBuffer ib;
         int[] indexes;
-        public Game_map_portion[] portions;
         public int[] Size = new int[2];
 
 
@@ -34,54 +35,36 @@ namespace Test
             Size[0] = (int)(WANOK.PORTION_RADIUS * WANOK.SQUARESIZE);
             Size[1] = (int)(WANOK.PORTION_RADIUS * WANOK.SQUARESIZE);
 
-            // Init portions
-            portions = new Game_map_portion[WANOK.PORTION_RADIUS*WANOK.PORTION_RADIUS];
-            int k = 0;
-            for (int i = 0; i < WANOK.PORTION_RADIUS; i++)
-            {
-                for (int j = 0; j < WANOK.PORTION_RADIUS; j++)
-                {
-                    /*
-                    portions[k] = new Game_map_portion(i*WANOK.PORTIONSIZE,j*WANOK.PORTIONSIZE);
-                    string json = JsonConvert.SerializeObject(portions[k]);
-                    FileStream fs = new FileStream("Content/Datas/Maps/MAP0001/"+i+"-"+j+".JSON", FileMode.Create);
-                    StreamWriter sw = new StreamWriter(fs);
-                    sw.WriteLine(json);
-                    sw.Close();
-                    fs.Close();
-                    */
-                    FileStream fs = new FileStream("Content/Datas/Maps/MAP0001/" + i + "-" + j + ".JSON", FileMode.Open);
-                    StreamReader sr = new StreamReader(fs);
-                    string json = sr.ReadToEnd();
-                    portions[k] = JsonConvert.DeserializeObject<Game_map_portion>(json);
-                    k++;
-                }
-            }
-
             // Building vertex buffer indexed
-            this.vertexList = new List<VertexPositionTexture>();
+            this.verticesList = new List<VertexPositionTexture>();
             this.indexesList = new List<int>();
             this.indexes = new int[]
             {
                 0, 1, 2, 0, 2, 3
             };
-            int offset = 0;
-            k = 0;
+            int offset = 0, k = 0;
+            Game_map_portion portion;
             for (int i = 0; i < WANOK.PORTION_RADIUS; i++)
             {
                 for (int j = 0; j < WANOK.PORTION_RADIUS; j++)
                 {
-                    for (int l = 0; l < portions[k].texture_floors.Count; l++)
+                    // Reading each portions
+                    FileStream fs = new FileStream("Content/Datas/Maps/MAP0001/" + i + "-" + j + ".JSON", FileMode.Open);
+                    StreamReader sr = new StreamReader(fs);
+                    string json = sr.ReadToEnd();
+                    portion = JsonConvert.DeserializeObject<Game_map_portion>(json);
+
+                    for (int l = 0; l < portion.texture_floors.Count; l++)
                     {
-                        for (int m = 0; m < portions[k].floors[l].Count; m++)
+                        for (int m = 0; m < portion.floors[l].Count; m++)
                         {
-                            foreach (VertexPositionTexture vertex in CreateFloorWithTex(portions[k].floors[l][m][0], portions[k].floors[l][m][1], portions[k].texture_floors[l]))
+                            foreach (VertexPositionTexture vertex in CreateFloorWithTex(portion.floors[l][m][0], portion.floors[l][m][1], portion.texture_floors[l]))
                             {
-                                vertexList.Add(vertex);
+                                this.verticesList.Add(vertex);
                             }
                             for (int n = 0; n < 6; n++)
                             {
-                                indexesList.Add(indexes[n] + offset);
+                                this.indexesList.Add(indexes[n] + offset);
                             }
                             offset += 4;
                         }
@@ -89,11 +72,12 @@ namespace Test
                     k++;
                 }
             }
-
-            this.ib = new IndexBuffer(this.device, IndexElementSize.ThirtyTwoBits, indexesList.Count, BufferUsage.None);
-            this.ib.SetData(indexesList.ToArray());
-            vb = new VertexBuffer(device, VertexPositionTexture.VertexDeclaration, vertexList.Count, BufferUsage.None);
-            vb.SetData(vertexList.ToArray());
+            this.verticesArray = this.verticesList.ToArray();
+            this.indexesArray = this.indexesList.ToArray();
+            this.ib = new IndexBuffer(this.device, IndexElementSize.ThirtyTwoBits, this.indexesArray.Length, BufferUsage.None);
+            this.ib.SetData(this.indexesArray);
+            vb = new VertexBuffer(device, VertexPositionTexture.VertexDeclaration, this.verticesArray.Length, BufferUsage.None);
+            vb.SetData(this.verticesArray);
         }
 
         // -------------------------------------------------------------------
@@ -133,19 +117,15 @@ namespace Test
 
         public void Draw(GameTime gameTime, BasicEffect effect)
         {
-            // Effeect settings
+            // Effect settings
             effect.Texture = Game1.currentFloorTex;
             effect.World = Matrix.Identity * Matrix.CreateScale(WANOK.SQUARESIZE, 1.0f, WANOK.SQUARESIZE);
 
-            // Converting to array
-            VertexPositionTexture[] verticesArray = this.vertexList.ToArray();
-            int[] indexesArray = this.indexesList.ToArray();
-
-
+            // Drawing
             foreach (EffectPass pass in effect.CurrentTechnique.Passes)
             {
                 pass.Apply();
-                this.device.DrawUserIndexedPrimitives(PrimitiveType.TriangleList, verticesArray, 0, verticesArray.Length, indexesArray, 0, verticesArray.Length / 2);
+                this.device.DrawUserIndexedPrimitives(PrimitiveType.TriangleList, this.verticesArray, 0, this.verticesArray.Length, this.indexesArray, 0, this.verticesArray.Length / 2);
             }
         }
     }
